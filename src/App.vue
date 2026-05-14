2fcb81109e41f722be7276f0b269c24f6877ee6b
kostyasabada <ksabada@mycubes.nl>
2026-05-14 13:15:27 +0300

    tailwind

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,3 @@
-<script>
-  export default {
-    data() {
-      return {
-        name: 'john doe',
-        status: true,
-        tasks: ['Task 1', 'Task 2', 'Task 3']
-      };
-    }
-  };
-</script>
-
 <template>
-  <h1>Hello, {{ name }}!</h1>
-  <p v-if="status">Status: Active</p>
-  <p v-else>Status: Inactive</p>
-
-  <h3>Tasks</h3>
-  <ul>
-    <li v-for="task in tasks" :key="task">{{ task }}</li>
-  </ul>
+  <h1 class="text-2xl">vue jobs</h1>
 </template>
