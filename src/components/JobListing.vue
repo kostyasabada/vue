070bfa19cb66197ea4a81ab2cfbc69a73390bbbb
kostyasabada <ksabada@mycubes.nl>
2026-05-14 17:24:12 +0300

    jobView

--- src/components/JobListing.vue
+++ src/components/JobListing.vue
@@ -1,4 +1,5 @@
 <script setup>
+import { RouterLink } from 'vue-router';
 import { defineProps, ref, computed } from 'vue';
 const props = defineProps({
   job: {
@@ -35,7 +36,7 @@ const truncatedDescription = computed(() => {
         <div>
           {{ truncatedDescription }}
         </div>
-        <button @click="toggleFullDescription" class="text-green-500 hover:text-green-600 mb-5" >
+        <button @click="toggleFullDescription" class="text-green-500 hover:text-green-600 mb-5">
           {{ showFullDescription ? 'Less' : 'More' }}
         </button>
       </div>
@@ -49,10 +50,10 @@ const truncatedDescription = computed(() => {
           <i class="pi pi-map-marker text-orange-700"></i>
           {{ job.location }}
         </div>
-        <a :href="'/job/' + job.id"
+        <RouterLink :to="'/jobs/' + job.id"
           class="h-[36px] bg-green-500 hover:bg-green-600 text-white px-4 py-2 rounded-lg text-center text-sm">
           Read More
-        </a>
+        </RouterLink>
       </div>
     </div>
   </div>
