820676247689a47b85671503b90f768de2a4a893
kostyasabada <ksabada@mycubes.nl>
2026-05-14 18:28:42 +0300

    added proxy

--- src/components/JobListings.vue
+++ src/components/JobListings.vue
@@ -1,8 +1,9 @@
 <script setup>
 import { RouterLink } from 'vue-router';
 import JobListing from './JobListing.vue';
-import jobData from '@/jobs.json';
-import { ref, defineProps } from 'vue';
+import { reactive, defineProps, onMounted } from 'vue';
+import { PulseLoader } from 'vue-spinner';
+import axios from 'axios';
 
 defineProps({
     limit: {
@@ -14,8 +15,23 @@ defineProps({
     }
 })
 
-const jobs = ref(jobData);
-console.log(jobs.value);
+const state = reactive({
+    jobs: [],
+    isLoading: true
+});
+
+onMounted(async () => {
+    try {
+        const response = await axios.get('/api/jobs');
+        console.log('Fetched jobs:', response.data);
+        state.jobs = response.data;
+    } catch (error) {
+        console.error('Error fetching jobs:', error);
+    } finally {
+        state.isLoading = false;
+    }
+
+});
 
 </script>
 
@@ -23,8 +39,11 @@ console.log(jobs.value);
     <section class="bg-blue-50 px-4 py-10">
         <div class="container-xl lg:container m-auto">
             <h2 class="text-3xl font-bold text-green-500 mb-6 text-center">Brows jobs</h2>
-            <div class="grid grid-cols-1 md:grid-cols-3 gap-6">
-                <JobListing v-for="job in jobs.slice(0, limit || jobs.length)" :key="job.id" :job="job" />
+            <div v-if="state.isLoading" class="text-center text-gray-500 py-6">
+                <PulseLoader :loading="state.isLoading" color="#3b82f6" size="15px" />
+            </div>
+            <div v-else class="grid grid-cols-1 md:grid-cols-3 gap-6">
+                <JobListing v-for="job in state.jobs.slice(0, limit || state.jobs.length)" :key="job.id" :job="job" />
             </div>
         </div>
     </section>
