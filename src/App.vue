3aa64ef42d84540a2893a0613f860ac54864075c
kostyasabada <ksabada@mycubes.nl>
2026-05-14 11:23:53 +0300

    first commit

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<script>
+  export default {
+    data() {
+      return {
+        name: 'john doe',
+        status: true,
+        tasks: ['Task 1', 'Task 2', 'Task 3']
+      };
+    }
+  };
+</script>
+
+<template>
+  <h1>Hello, {{ name }}!</h1>
+  <p v-if="status">Status: Active</p>
+  <p v-else>Status: Inactive</p>
+
+  <h3>Tasks</h3>
+  <ul>
+    <li v-for="task in tasks" :key="task">{{ task }}</li>
+  </ul>
+</template>
